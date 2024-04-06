c636129b53f963ad29d944dc70e9ecdf21571007
csurul <cuneytsurul@hotmail.com>
2024-04-06 15:20:28 +0300

    change some js file directories

---
+++ 1.0 Denemelerrr/JS Workouts/fizbuzz.js
@@ -0,0 +1,17 @@
+function whosPaying(names) {
+    
+    /******Don't change the code above*******/
+        
+        //Write your code here.
+        var name = names[Math.floor(Math.random() * (names.length))]
+        return name + " is going to buy lucnh today!";
+        
+        
+        
+    
+    
+    /******Don't change the code below*******/    
+    }
+
+    var names = ["den1", "den2", "den3", "den4", "den5", "den6", "den7"];
+    console.log(whosPaying(names));
