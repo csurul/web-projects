4d4655a64d6fe63fb8afc17c6482c46bbb3ec2fc
csurul <cuneytsurul@hotmail.com>
2024-04-11 11:08:34 +0300

    update cmc api project

--- CMC API Project/index.js
+++ CMC API Project/index.js
@@ -1,19 +1,22 @@
 import express from "express";
-//import bodyParser from "body-parser";
+import bodyParser from "body-parser";
 import axios from "axios";
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-import fse from "fs";
 
-const parentFolder = __dirname.substring(0, __dirname.lastIndexOf("\\"));
+
+import fs from "fs";
+
+const parentFolder = __dirname.substring(0, __dirname.lastIndexOf("/"));
+//const parentFolder = __dirname.substring(0, __dirname.lastIndexOf("\\"));
 const filePath = "/Local Workouts/1_CMC_APIKEY.txt";
 let apiKey = null;
 
 //read the coinmarketcap apikey from a file located in another folder
-fse.readFile(parentFolder + filePath, {encoding: 'utf-8'}, function(err, data){
+fs.readFile(parentFolder + filePath, {encoding: 'utf-8'}, function(err, data){
     if (!err) {
         apiKey = data;
     } else {
@@ -21,20 +24,11 @@ fse.readFile(parentFolder + filePath, {encoding: 'utf-8'}, function(err, data){
     }
 });
 
-/*fse.read(__dirname + "/1_CMC_APIKEY.txt", Buffer.alloc(100), function(err,data){
-    if (!err) {
-        console.log('received data: ' + data);
-        
-    } else {
-        console.log(err);
-    }
-});*/
-
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let result = null;
 let result2 = null;
@@ -44,41 +38,55 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/list_exchange", async (req, res) => {
-  try {
-    result = await axios.get('https://pro-api.coinmarketcap.com/v1/exchange/map?limit=10&&sort=volume_24h', {
-      headers: {
-        'X-CMC_PRO_API_KEY': apiKey,
-      },
-    });
-  } catch (error) {
-    result = null;
-    res.status(500).send(error.message);
-  }
   
-  if (result) {
-    console.log(result.data);
-    res.render("index.ejs", { data: result.data.data });
-  } else {
-    res.render("index.ejs", { data: "Response is undefined" });
-  }
   
 });
 
+async function ListExchange() {
+  
+}
+
+let resultMetrics;
+let resultCryptos;
+let resultExchanges;
+
 app.post("/map_cryptocurreny", async (req, res) => {
   try {
-    result = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?sort=cmc_rank&&limit=10', {
+    resultMetrics = await axios.get('https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest', {
       headers: {
         'X-CMC_PRO_API_KEY': apiKey,
       },
     });
   } catch (error) {
-    result = null;
+    resultMetrics = null;
+    res.status(500).send(error.message);
+  }
+  
+  try {
+    resultExchanges = await axios.get('https://pro-api.coinmarketcap.com/v1/exchange/map?limit=10&&sort=volume_24h', {
+      headers: {
+        'X-CMC_PRO_API_KEY': apiKey,
+      },
+    });
+  } catch (error) {
+    resultExchanges = null;
+    res.status(500).send(error.message);
+  }
+
+  try {
+    resultCryptos = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?sort=cmc_rank&&limit=10', {
+      headers: {
+        'X-CMC_PRO_API_KEY': apiKey,
+      },
+    });
+  } catch (error) {
+    resultCryptos = null;
     res.status(500).send(error.message);
   }
   
   let cryptoInfoLink = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id="
   let cryptoIDS = [];
-  result.data.data.forEach(element => {
+  resultCryptos.data.data.forEach(element => {
     cryptoIDS.push(element.id);
     cryptoInfoLink += element.id + ",";
   });
@@ -92,15 +100,15 @@ app.post("/map_cryptocurreny", async (req, res) => {
       },
     });
   } catch (error) {
-    result = null;
+    resultCryptos = null;
     res.status(500).send(error.message);
   }
 
   if (result2) {
-    console.log(result2.data.data);
+    //console.log(result2.data.data);
     //console.log(result2.data.data[cryptoIDS[1]].id + result2.data.data[cryptoIDS[1]].symbol);
    
-    res.render("index.ejs", { data: result2.data.data, ids: cryptoIDS });
+    res.render("index.ejs", { metrics: resultMetrics.data.data, exchanges: resultExchanges.data.data, data: result2.data.data, ids: cryptoIDS });
   } else {
     res.render("index.ejs", { data: "Response is undefined" });
   }
@@ -116,9 +124,10 @@ app.post("/map_cryptocurreny", async (req, res) => {
 });
 
 
-app.post("/cryptocurreny_info", async (req, res) => {
+app.post("/search_crypto", async (req, res) => {
   try {
-    result = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=1', {
+    console.log(req.body.slug); 
+    result = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?slug=' + req.body.slug, {
       headers: {
         'X-CMC_PRO_API_KEY': apiKey,
       },
@@ -130,9 +139,9 @@ app.post("/cryptocurreny_info", async (req, res) => {
   
   if (result) {
     console.log(result.data);
-    //res.render("index.ejs", { data: result.data.data });
+    res.render("crypto.ejs", { data: result.data.data });
   } else {
-    res.render("index.ejs", { data: "Response is undefined" });
+    res.render("crypto.ejs", { data: "Response is undefined" });
   }
   
 });
