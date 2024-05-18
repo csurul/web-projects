b794a58dfb31de80f3f85bd44973a4fffcf14ae3
csurul <cuneytsurul@hotmail.com>
2024-05-18 17:51:08 +0300

    change family tracker project

--- 8.5 Family Travel Tracker/index2.js
+++ 8.5 Family Travel Tracker/index2.js
@@ -12,6 +12,7 @@ const db = new pg.Client({
   password: "569103",
   port: 5432,
 });
+
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,16 +20,14 @@ app.use(express.static("public"));
 
 let currentUserId = 1;
 
-let users = [
-  { id: 1, name: "Angela", color: "teal" },
-  { id: 2, name: "Jack", color: "powderblue" },
-];
+let users = [];
 
 async function checkVisisted() {
   const result = await db.query(
-    "SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1; ",
+    "select country_code from visited_countries join users on users.id = user_id where user_id = $1;", 
     [currentUserId]
   );
+  
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
@@ -37,7 +36,7 @@ async function checkVisisted() {
 }
 
 async function getCurrentUser() {
-  const result = await db.query("SELECT * FROM users");
+  const result = await db.query("SELECT * FROM users order by id");
   users = result.rows;
   return users.find((user) => user.id == currentUserId);
 }
@@ -45,6 +44,7 @@ async function getCurrentUser() {
 app.get("/", async (req, res) => {
   const countries = await checkVisisted();
   const currentUser = await getCurrentUser();
+
   res.render("index.ejs", {
     countries: countries,
     total: countries.length,
@@ -63,30 +63,33 @@ app.post("/add", async (req, res) => {
       [input.toLowerCase()]
     );
 
-    const data = result.rows[0];
-    const countryCode = data.country_code;
-    try {
-      await db.query(
-        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
-        [countryCode, currentUserId]
-      );
-      res.redirect("/");
-    } catch (err) {
-      console.log(err);
+    if (result != null) {
+      const data = result.rows[0];
+      const countryCode = data.country_code;
+
+      try {
+        await db.query("insert into visited_countries (country_code, user_id) values ($1, $2);",
+              [countryCode, currentUser.id]);
+
+        res.redirect("/");
+      } catch (error) {
+        console.log(err);
+      }
     }
-  } catch (err) {
+    
+  } catch (error) {
     console.log(err);
   }
 });
 
-app.post("/user", async (req, res) => {
-  if (req.body.add === "new") {
+app.post("/user", async(req, res) => {
+  if(req.body.add === "new") {
     res.render("new.ejs");
   } else {
     currentUserId = req.body.user;
     res.redirect("/");
   }
-});
+})
 
 app.post("/new", async (req, res) => {
   const name = req.body.name;
@@ -104,5 +107,5 @@ app.post("/new", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port: ${port}`);
 });
